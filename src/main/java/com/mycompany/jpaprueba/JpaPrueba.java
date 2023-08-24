17564d3d14504e2892e7088699d807e411079124
NicolasPozzer <pozzernico@gmail.com>
2023-08-23 23:20:00 -0300

    publis-in-github

---
+++ src/main/java/com/mycompany/jpaprueba/JpaPrueba.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.jpaprueba;
+
+import com.mycompany.jpaprueba.persistence.ControladoraPersistencia;
+
+public class JpaPrueba {
+
+    public static void main(String[] args) {
+        
+        //Llamamos la controladora de Persistencia
+        ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+        
+        
+    }
+}
---
+++ src/main/java/com/mycompany/jpaprueba/persistence/ControladoraPersistencia.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.jpaprueba.persistence;
+
+//Controladora de todas nuestras persistencias
+public class ControladoraPersistencia {
+    
+    //Declaramos Todas las unidades de persistencia que tenemos
+    AlumnoJpaController aluJpa = new AlumnoJpaController();
+    
+}
